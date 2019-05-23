2770eca8b56cd8ebf46c1de32465da2b49f07597
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-05-23 06:26:37 +0500

    Add 2.3 - Dataset and empty .py file

---
+++ 2_regression/2_3_polynomial/poly_regression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 23 06:23:04 2019
+
+@author: saadmashkoor
+"""
+"""poly_regression.py - an implementation of polynomial regression in Python.
+This is the first non-linear regression model of the course."""
+
+"""DATA PREPROCESSING"""
