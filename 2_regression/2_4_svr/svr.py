b4cb72a180e62f67f3f3e67b423d0454520ef3d5
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-05-25 06:14:58 +0500

    Add 2.4 - Empty svr.py program and dataset

---
+++ 2_regression/2_4_svr/svr.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 06:13:43 2019
+
+@author: saadmashkoor
+"""
+
+"""svr.py - Support Vector Regression model for an arbitrary dataset."""
